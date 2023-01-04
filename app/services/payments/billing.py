7a68f9a7e4b12be18150c840555c0d2a3e82699f
devkarych <kaarych@gmail.com>
2023-01-05 00:27:52 +0300

    Billing fixes. It works!

--- app/services/payments/billing.py
+++ app/services/payments/billing.py
@@ -24,33 +24,25 @@ class PaymentsChecker:
     async def check(self) -> None:
         dao = UserDAO(session=self.bot.get("db"))
         async with YooMoneyAPI(api_access_token=self.yoomoney_token) as client:
-            payments_history = await client.operation_history(records=100)
             for user in await dao.get_all():
+                payments_history = await client.operation_history(records=100, label=str(user.id))
                 new_payment = self._get_new_payment_if_exists(payments_history=payments_history, user=user)
-                print('Нью')
-                print(new_payment)
                 if new_payment:
-                    print('инкремент')
                     await dao.increment_payments_count(user_id=user.id)
-                    print(await dao.get_by_id(user.id))
-                    print('Новая оплата')
-                    print(new_payment)
-                    print(user)
 
     def _get_new_payment_if_exists(self, payments_history: OperationHistory, user: User) -> Optional[Operation]:
         personal_operations = []
         for operation in payments_history.operations:
-            if str(user.id) in str(operation.label) and operation.status == "success":
+            if operation.status == "success" and operation.label == str(user.id) and operation.direction == 'in':
                 personal_operations.append(operation)
 
         if personal_operations:
             # Skipped user without new (actual) payments
-            print(personal_operations)
             if len(personal_operations) <= user.payments_count:
                 return None
-            print(personal_operations, user.payments_count)
             for operation in personal_operations:
-                if operation.status == "success" and get_payment_type(amount=operation.amount):
+                print(operation.label)
+                if get_payment_type(amount=operation.amount):
                     return operation
         return None
 
@@ -62,5 +54,5 @@ def get_payment_url(user_id: int, cost: Cost) -> str:
         targets="Hideline",
         payment_type="SB",
         amount=cost.value,
-        label=f"{user_id}_{cost.value}"
+        label=str(user_id)
     )
